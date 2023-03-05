119dab6cf52d806f0e6498958ae5e3605da3f496
ry-na <ryo.natori0809@gmail.com>
2023-03-05 17:58:28 +0900

    try to change language from en to ja

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: "スマイルジャーニー",
     description:
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
-    language: "ja",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
